03188303a557414af2753ba59d16a18d907127c1
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-03-10 06:05:08 -0700

    styling Book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,6 @@
 export default function Book({title, thumbnail}) {
     return(
-        <div className="book mx-3 mt-5">
+        <div className="book mx-3 mt-5 flex items-center">
             <img src={thumbnail} alt={title} />
         </div>
     )
